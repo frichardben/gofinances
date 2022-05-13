4e802845c800daae73c7411d72138c3826367b5f
Richard Ben <richardben@yahoo.com.br>
2022-05-13 08:57:04 -0300

    feat: adding measure proportions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+    Container, 
+    Header,
+    UserWrapper, 
+    UserInfo, 
+    Photo, 
+    User, 
+    UserGreeting,
+    UserName 
+} from './styles';
 
 export function Dashboard() {
     return(
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/24980034?v=4', }}
+                        />
+                        <User>
+                            <UserGreeting> Olá,</UserGreeting>
+                            <UserName>Richard</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
