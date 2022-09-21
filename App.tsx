5a8c79da4a94121d7aeb501e96069612ca8aca60
Richard Ben <frichardben@yahoo.com.br>
2022-09-21 14:01:13 -0300

    chore: added tab navigation

--- App.tsx
+++ App.tsx
@@ -9,10 +9,10 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins'
 
+import { NavigationContainer } from '@react-navigation/native'
+import AppRoutes from './src/routes/routes'
+
 import theme from './src/global/styles/theme'
-// import Dashboard from './src/screens/Dashboard'
-import Register from './src/screens/Register'
-// import CategorySelect from './src/screens/CategorySelect'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
